a9c0836d0be3727e0bd862271b553e18d2cf5130
James Li <34687158+j4mesli@users.noreply.github.com>
2023-01-25 20:23:18 -0500

    updated Recipe Book

--- Recipe Book/src/app/recipes/recipe-list/recipe-list.component.ts
+++ Recipe Book/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -8,9 +8,9 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipeListComponent {
   // properties
-  recipes = [
+  recipes: Recipe[] = [
     new Recipe('Test Recipe', 'init test recipe', 'https://people.com/thmb/Q1GBj0_zq95hHmypVkxnGUw12sI=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc():focal(693x0:695x2)/panera-sandwiches-010623-c326f0d822b2440cb74ed9524913ed5e.jpg'), 
-  ] as Recipe[];
+  ];
 
   // methods
   
